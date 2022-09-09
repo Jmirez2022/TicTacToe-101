35e97feb3228d989b1ed14eed7472bc023f3f176
Jmirez2022 <Jazi.2000@hotmail.com>
2022-09-08 20:26:17 -0500

    fixed errors

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@
     // next to each @TODO you will find tasks that need to be finished
 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
-let currentMarker = 'X'
+let currentMarker = "X"
 
 
 
@@ -15,6 +15,14 @@ let currentMarker = 'X'
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
+  console.log('element:', element)
+  console.log('element.id:', element.id)
+  element.style.backgroundColor = 'red'
+  element.style.color = 'white'
+  // element.style.display = 'flex'
+  element.style.justifyContent = 'center'
+  element.style.alignItems = 'center'
+  element.style.fontSize = '32px'
 
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
   console.log(`The element you clicked on has an id:  ${element.id}`)
@@ -25,26 +33,21 @@ const handleClick = (element) => {
     addMarker(element.id)
   }
 }
+const handleBoxClick = (element) => {
+  // const box1 = document.getElementById('box-1')
 
-
-
-
-
-
-
-
-
+}
 
 
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
-
+console.log("id", id)
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
+  document.getElementById(id).innerHTML = currentMarker
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
   // .getElementById(id)
@@ -53,16 +56,6 @@ const addMarker = (id) => {
 
   changeMarker()
 }
-
-
-
-
-
-
-
-
-
-
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
   if(currentMarker === "X"){
@@ -71,19 +64,11 @@ const changeMarker = () => {
     currentMarker = "X"
   }
 }
-
-
-
-
-
-
-
-
-
-
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
   
+  // element.style.backgroundColor = 'white'
+  const squares = document.getElementsByTagName("TD") 
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
       // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
     
@@ -99,7 +84,7 @@ const resetBoard = () => {
 
     // will log out the id of each square as it loops over them.
     console.log(squares[i].id)
-
+    document.getElementById(squares[i].id).style.backgroundColor = 'white'
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
   }  
